b68c484f1b1c75ff124eb01113518fc0aa281586
Jared Goldberg <jaredgold@comcast.net>
2017-09-28 19:57:11 -0400

    Update Tweet_Post_Analysis.py

--- Tweet_Post_Analysis.py
+++ Tweet_Post_Analysis.py
@@ -32,23 +32,13 @@ if not client_secret or not client_key:
 def get_tokens():
     ## Step 1: Obtain a request token which will identify you (the client) in the next step.
     # At this stage you will only need your client key and secret
-
-    # OAuth1Session is a class defined in the requests_oauthlib module
-    # Two values are passed to the __init__ method of OAuth1Session
-    # -- the key is passed as the value of the first parameter (whose name we don't know)
-    # -- the secret is passed as the value of the parameter that is also called client_secret
     # after this line executes, oauth will now be an instance of the class OAuth1Session
     oauth = requests_oauthlib.OAuth1Session(client_key, client_secret=client_secret)
 
     request_token_url = 'https://api.twitter.com/oauth/request_token'
 
     # invoke the fetch_request_token method of the class OAuth1Session on our instance
-    # it returns a dictionary that might look like this:
-    # {
-    #     "oauth_token": "Z6eEdO8MOmk394WozF5oKyuAv855l4Mlqo7hhlSLik",
-    #     "oauth_token_secret": "Kd75W4OQfb2oJTV0vzGzeXftVAwgMnEK9MumzYcM"
-    # }
-    # It also saves the oauth_token as an instance variable of the object
+    # it returns a dictionary
     # oauth is bound to, so it can be used in later steps
     fetch_response = oauth.fetch_request_token(request_token_url)
 
@@ -57,7 +47,7 @@ def get_tokens():
     resource_owner_key = fetch_response.get('oauth_token')
     resource_owner_secret = fetch_response.get('oauth_token_secret')
 
-    ## Step 2. Obtain authorization from the user (resource owner) to access their protected resources (images, tweets, etc.). This is commonly done by redirecting the user to a specific url to which you add the request token as a query parameter. Note that not all services will give you a verifier even if they should. Also the oauth_token given here will be the same as the one in the previous step.
+    ## Step 2. Obtain authorization from the user (resource owner) to access 
 
     base_authorization_url = 'https://api.twitter.com/oauth/authorize'
     # append the query parameters need to make it a full url.
@@ -68,25 +58,12 @@ def get_tokens():
 
     webbrowser.open(authorization_url)
 
-    # After the user authenticates at Twitter, it would normally "redirect"
-    # the browser back to our website. But we aren't running a website.
-    # Some services, like Twitter, will let you configure the app to
-    # display a verifier, or the entire redirect url, rather than actually
-    # redirecting to it.
+  
     # User will have to cut and paste the verifier or the whole redirect url
 
     # version where the website provides a verifier
     verifier = raw_input('Please input the verifier>>> ')
 
-    # version where the website provides the entire redirect url
-    # redirect_response = raw_input('Paste the full redirect URL here: ')
-    # oauth_response = oauth.parse_authorization_response(redirect_response)
-    # get back something like this
-    #{
-    #    "oauth_token": "Z6eEdO8MOmk394WozF5oKyuAv855l4Mlqo7hhlSLik",
-    #    "oauth_verifier": "sdflk3450FASDLJasd2349dfs"
-    #}
-    # verifier = oauth_response.get('oauth_verifier')
 
     ## Step 3. Obtain an access token from the OAuth provider. Save this token so it can be re-used later.
     # In this step we re-use most of the credentials obtained up to this point.
@@ -100,11 +77,7 @@ def get_tokens():
 
     access_token_url = 'https://api.twitter.com/oauth/access_token'
     oauth_tokens = oauth.fetch_access_token(access_token_url)
-    # get back something like this
-    #{
-    #    "oauth_token": "6253282-eWudHldSbIaelX7swmsiHImEL4KiNdrY",
-    #    "oauth_token_secret": "2EEfA6BG3ly3sR3RjE0IBSnKmrkVU"
-    #}
+ 
     resource_owner_key = oauth_tokens.get('oauth_token')
     resource_owner_secret = oauth_tokens.get('oauth_token_secret')
 
@@ -128,33 +101,19 @@ except:
 
 ## Step 4. Access protected resources.
 
-# For endpoints that might be interesting to try, see
-# https://dev.twitter.com/rest/tools/console and
-# https://dev.twitter.com/rest/public
 protected_url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
 oauth = requests_oauthlib.OAuth1Session(client_key,
                         client_secret=client_secret,
                         resource_owner_key=resource_owner_key,
                         resource_owner_secret=resource_owner_secret)
 
-# Call the get method. The work of encoding the client_secret
-# and "signing" the request is taken care of behind the scenes.
-# The results are also processed for you, including calling .read() and
-# encoding as json.
-#r = oauth.get(protected_url)
-# r is now an instance of the Response class in the requests module
-# documentation at
-# http://docs.python-requests.org/en/latest/user/quickstart/#response-content
-
-# Of particular interest to us is the json() method of the Response class
-#print pretty(r.json())
 #raw_input("Enter twitter the screen name you wish to analyze: ")
 twitter_screen_name = "FoxNews"
 # tweets_count = raw_input("Enter how many tweets you want: ")
 tweets_count = 200
 params_dic = {"screen_name": twitter_screen_name, "count": tweets_count}
 
-#CACHING CODE MODIFIED FROM TEXTBOOK will take data from API if not already cached, or will take from cached data
+#CACHING CODE will take data from API if not already cached, or will take from cached data
 cache_fname = "twitter_cached_results.txt"
 try:
     fobj = open(cache_fname, 'r')
@@ -197,20 +156,6 @@ result_text = get_with_caching(protected_url, params_dic, saved_cache, cache_fna
 json_tweets = json.loads(result_text) #this turns results into json
 #pprint.pprint(json_tweets)
 
-
-#r = oauth.get("https://api.twitter.com/1.1/statuses/user_timeline.json", params = params_dic) # request to the Tweet search endpoint, searching for the phrase 'University of Michigan', looking to get 3 Tweets back
-
-# investigate the data
-# print type(r.json())
-# print json.dumps(r.json(), indent=2)
-# return_data = r.json()
-# pprint.pprint(return_data)
-# print res.keys()
-# # print pretty(res)
-# # cache data
-# f = open('nested.txt', 'w')
-# f.write(json.dumps(res))
-# f.close()
 
 #this is creating sentiment lists
 pos_ws = []
@@ -320,12 +265,6 @@ def supportive(leanscore): #sees whether more tweets were positive or negative, 
 access_token = raw_input("Please enter an access token!\n Check out developers.facebook.com/tools/explorer if unknown: ")
 facebook_user_ID = 346937065399354 #raw_input("Please enter the Facebook user ID that you want to analyze here!\n Check out http://findmyfbid.com/ if unknown: ")
 ####The following code allows us to grade your code with graders' tokens. (It also helps you run the code if your access token has expired.) Please do not change it! 
-
-# print r.status_code
-# if r.status_code != 200:
-#     access_token = raw_input("Get a Facebook access token v2.3 from https://developers.facebook.com/tools/explorer and enter it here if the one saved in the file doesn't work anymore.  :\n")
-# facebook_user_ID = raw_input("Paste the Facebook user ID that you want to analyze here! Check out http://findmyfbid.com/ if unknown: ")
-
 
 FB_url = "https://graph.facebook.com/v2.3/{}/posts".format(facebook_user_ID)
 
@@ -460,22 +399,7 @@ for each in sorted_facebook_leaning_dict[:3]:
     print ""
 print supportive1(overall_post_lean) 
 
-# class Hi(unittest.TestCase):
 
-#     def test_json(self):
-#         xy = json_tweets
-#         self.assertEqual(type(xy), type("hi") #testing that json_tweets returns a string
-#     # def test_presidential_tweet(self):
-#     #     self.assertTrue(Tweet({"text": "Trump is great"}).PE_tweet() == True) #testing that PE_tweet is working correctly
-#     # def test_tweet_message(self):
-#     #     self.assertEqual(type(y.message), type("message")) #this is testing that Tweet class, message instance was created correctly
-#     # def test_supportive_test(self):
-#     #     self.assertEqual(supportive(0),"Overall, the tweets indicate a NEUTRAL view of our next president!") #testing that supprtive is working correctly
-#     # def test_FB_instance(self):
-#     #     self.assertEqual(type(facebook_instance_list[0].PE_post()),type(False)) #testing that PE_tweet is giving correct output type
-#     # def test_6(self):
-#     #     self.assertTrue("true" == "true")
-# unittest.main(verbosity=2)
 print ""
 print ""
 print ""
@@ -494,10 +418,3 @@ class AllMyTests(unittest.TestCase):
     def test_json_length(self):
         self.assertEqual(len(facebook_instance_list), 200)
 unittest.main(verbosity = 2)
-
-
-
-
-
-
-
